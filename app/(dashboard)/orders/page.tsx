0c00124c3be0730f01934a835cc10f0bc1fd0cb0
Soumya Ranjan Nanda <srnranjan2005@gmail.com>
2026-02-05 00:18:04 +0530

    DB updated

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -2,6 +2,8 @@ import { getOrders, type OrderWithItems } from "@/lib/watchService";
 import { deleteOrder } from "./actions";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function OrdersPage() {
 
   const orders = (await getOrders()) as OrderWithItems[];
